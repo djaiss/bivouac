19d3a5209d1d88aa82fe9c0332d6ab6236ef3a86
Regis Freyd <djaiss@users.noreply.github.com>
2023-06-23 10:26:29 -0400

    Update OfficeFactory.php

--- database/factories/OfficeFactory.php
+++ database/factories/OfficeFactory.php
@@ -5,6 +5,9 @@ namespace Database\Factories;
 use App\Models\Organization;
 use Illuminate\Database\Eloquent\Factories\Factory;
 
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Office>
+ */
 class OfficeFactory extends Factory
 {
     /**
